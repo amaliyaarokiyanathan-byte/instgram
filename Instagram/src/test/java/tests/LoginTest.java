dcdfcdff00ae8d2195a2018626fbd7c90f87e0c7
amaliyaarokiyanathan-byte <amaliyaarokiyanathan@gmail.com>
2026-05-22 08:21:59 +0530

    1 commit

--- Instagram/src/test/java/tests/LoginTest.java
+++ Instagram/src/test/java/tests/LoginTest.java
@@ -1,17 +1,32 @@
-package tests;
+	package tests;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
-import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
     @Test
     public void instagramLoginTest() throws InterruptedException {
 
+        // Setup ChromeOptions (CI-safe)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        // Initialize driver with options
+        driver = new ChromeDriver(options);
+
+        driver.get("https://www.instagram.com/");
+
         LoginPage loginPage = new LoginPage(driver);
 
-        Thread.sleep(5000); // wait for page load
+        Thread.sleep(5000);
 
         loginPage.login("your_username", "your_password");
 
